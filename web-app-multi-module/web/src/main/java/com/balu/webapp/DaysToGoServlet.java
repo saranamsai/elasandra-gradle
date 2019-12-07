3a18a4f55fc0e76bd561cbeb9286f8f6dd1cac4c
Mithun Technologies <devopstrainingblr@gmail.com>
2019-12-07 16:57:52 +0530

    tt

---
+++ web-app-multi-module/web/src/main/java/com/balu/webapp/DaysToGoServlet.java
@@ -0,0 +1,19 @@
+package com.balu.webapp;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+import com.balu.apputils.DateUtils;
+
+public class DaysToGoServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println(new DateUtils().daysToNewYear() + " days remaining in this year. Learn Gradle now!");
+	}
+}
